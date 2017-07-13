f7505ce05d2efe680378704d35e951cbeec73376
Vitali Douliez <vitali.roubtsov@hotmail.fr>
2017-07-13 18:51:14 +0200

    Play first version. Trident rough pitch and yaw, rough acceleration. Working acc U. Tail UI.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour {
+
+    public float rollSpeed = 1;
+    public float pitchSpeed = 1;
+
+    public float minSpeed = 0.01f;
+    public float maxSpeed = 0.03f;
+
+    public float accShift = 2;
+
+    public GameObject target;
+    public GameObject ps1, ps2;
+
+    public Image fuelGauge;
+    public Image plasmaGauge;
+
+    float fuel = 100;
+    float lastTimeFuelPressed = 0;
+    float plasma = 100;
+
+    void Update () {
+        transform.Rotate(Vector3.forward, -1 * Input.GetAxis("Horizontal") * rollSpeed);
+        transform.Rotate(Vector3.left, -1 * Input.GetAxis("Vertical") * pitchSpeed);
+
+        float accValue = Input.GetAxis("Accelerate");
+        float forwardSpeed = minSpeed;
+
+        ps1.transform.localScale = Vector3.one;
+        ps2.transform.localScale = Vector3.one;
+
+        target.transform.localPosition = new Vector3(0, 0.3f, 0);
+
+        if (accValue != 0)
+        {
+            if (fuel > accValue)
+            {
+                fuel -= accValue;
+                fuelGauge.fillAmount = fuel / 100;
+                lastTimeFuelPressed = Time.time;
+
+                ps1.transform.localScale = Vector3.one * (1 + 2 * accValue);
+                ps2.transform.localScale = Vector3.one * (1 + 2 * accValue);
+
+                target.transform.localPosition = new Vector3(0, 0.3f, -accValue * accShift);
+                forwardSpeed += (maxSpeed - minSpeed) * accValue;
+            }
+        }
+        else 
+        {
+            if (Time.time - lastTimeFuelPressed > 2 && fuel < 100)
+            {
+                fuel = Mathf.Clamp(fuel + 1, 0, 100);
+                fuelGauge.fillAmount = fuel / 100;
+            }
+        }
+
+        transform.Translate(Vector3.forward * forwardSpeed);
+	}
+}
---
+++ Assets/Scripts/Orbital.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbital : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 basePosition = new Vector3(0, 1, -3);
+    public float distance = 3.0f;
+    public float xStep = 1;
+    public float yStep = 1;
+    public float deltaSplit = 0.25f;
+
+    bool resetOk = true;
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            float xIn = Input.GetAxis("CamTestH");
+            float yIn = Input.GetAxis("CamTestV");
+
+            if (xIn != 0 || yIn != 0)
+            {
+                float x = -xIn * xStep;
+                float y = yIn * yStep;
+
+                float z = distance;
+
+                Vector3 pos = (new Vector3(x, y, z)).normalized * distance;
+                transform.localPosition = pos;
+                transform.LookAt(target.transform);
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+
+                resetOk = false;
+            }
+            else 
+            {
+                if (!resetOk)
+                {
+                    resetOk = true;
+                    transform.localPosition = basePosition;
+                    transform.rotation = new Quaternion();
+                }
+            }
+        }
+    }
+}
